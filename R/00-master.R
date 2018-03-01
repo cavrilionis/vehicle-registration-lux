97694dccaf0dbb7262066d8a658ce8d492770fa7
Christos Avrilionis <avrilionis@gmail.com>
2018-03-01 18:59:08 +0100

    Added clear console and new calls to R scripts

--- R/00-master.R
+++ R/00-master.R
@@ -3,6 +3,8 @@
 
 # This is the master program which executes all the others
 
+cat("\014")
+
 source(file.path("set-up.R"), echo = TRUE)
 
 source(file.path("R", "01-download-data.R"), echo = TRUE)
@@ -13,4 +15,6 @@ source(file.path("R", "03-set-column-labels.R"), echo = TRUE)
 
 source(file.path("R", "04-clean.R"), echo = TRUE)
 
-source(file.path("R", "05-explore.R"), echo = TRUE)
+source(file.path("R", "05-dictionary.R"), echo = TRUE)
+
+source(file.path("R", "06-explore.R"), echo = TRUE)
