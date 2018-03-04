fad96b03e4ce0bfbfe8c539afdd5a09eb05fac4e
cavrilionis <avrilionis@gmail.com>
2018-03-04 23:53:27 +0100

    Added code for frequency tables and graphs

--- R/06-explore.R
+++ R/06-explore.R
@@ -7,3 +7,34 @@ vehcln <- load(file.path("input", "vehcln.RData"))
 # Describe dataset
 describe(vehcln)
 
+# Frequency tables
+
+CATSTC2 <- summarise(group_by(vehcln, CATSTC), Freq = n())
+CATSTC2$CumFreq <- cumsum(CATSTC2$Freq)
+CATSTC2$Prop <- round(100 * prop.table(CATSTC2$Freq), 2)
+CATSTC2$CumProp <- round(cumsum(CATSTC2$Prop), 1)
+label(CATSTC2$Freq) <- "Frequency"
+label(CATSTC2$CumFreq) <- "Cumulative frequency"
+label(CATSTC2$Prop) <- "Percent"
+label(CATSTC2$CumProp) <- "Cumulative percent"
+
+CATSTC2
+
+CATSTC2$CATSTC[which(CATSTC2$Prop < 5)] <- "Other"
+
+ggplot(data = vehcln, aes(x = CATSTC)) +
+  geom_bar()
+
+ggplot(data = CATSTC2, aes(x = CATSTC, y = Prop)) +
+  geom_bar(stat = "identity")
+
+ggsave(plot = last_plot(),
+       path = "output",
+       filename = "CATSTC.png")
+
+# Mode (most frequent value) for nominal variables
+
+# Summary statistics of continuous variables
+
+# Histograms of continuous variables
+
