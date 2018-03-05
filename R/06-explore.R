2119707bca32400a7c15513b62d0a35a4e920feb
Christos Avrilionis <avrilionis@gmail.com>
2018-03-05 17:51:28 +0100

    Loop for generating frequency tables for categorical variables

--- R/06-explore.R
+++ R/06-explore.R
@@ -1,26 +1,75 @@
 
-########## 05-explore.R ##########
+cat("\014")
+rm(list = ls())
+
+########## 06-explore.R ##########
 
 # Get dataset
-vehcln <- load(file.path("input", "vehcln.RData"))
+load(file.path("input", "vehcln.RData"))
 
 # Describe dataset
 describe(vehcln)
 
+
+
+### SAVAS ###
+
+
+myCols <- dict$Column.ID[which(dict$Column.class == "character")]
+myCols
+
+for (i in seq_along(myCols)) {
+  names((vehcln[i]) <- as.data.frame(table(vehcln[i]))
+}
+
+
+
 # Frequency tables
 
-CATSTC2 <- summarise(group_by(vehcln, CATSTC), Freq = n())
-CATSTC2$CumFreq <- cumsum(CATSTC2$Freq)
-CATSTC2$Prop <- round(100 * prop.table(CATSTC2$Freq), 2)
-CATSTC2$CumProp <- round(cumsum(CATSTC2$Prop), 1)
-label(CATSTC2$Freq) <- "Frequency"
-label(CATSTC2$CumFreq) <- "Cumulative frequency"
-label(CATSTC2$Prop) <- "Percent"
-label(CATSTC2$CumProp) <- "Cumulative percent"
+freq_plot <- function(col_name) {
+  vehcln %>%
+    group_by_(col_name) %>%
+      summarise(Freq = n()) -> temp
+  temp$CumFreq <- cumsum(temp$Freq)
+  temp$Prop <- round(100 * prop.table(temp$Freq), 2)
+  temp$CumProp <- round(cumsum(temp$Prop), 1)
+  label(temp$Freq) <- "Frequency"
+  label(temp$CumFreq) <- "Cumulative frequency"
+  label(temp$Prop) <- "Percent"
+  label(temp$CumProp) <- "Cumulative percent"
+  temp <- arrange(temp, desc(Freq))
+  return(temp)
+}
 
-CATSTC2
+CATSTC <- freq_plot('CATSTC')
+LIBSTC <- freq_plot('LIBSTC')
+CODCAR <- freq_plot('CODCAR')
+LIBCAR <- freq_plot('LIBCAR')
+CATEU  <- freq_plot('CATEU')
+COUL   <- freq_plot('COUL')
+INDUTI <- freq_plot('INDUTI')
+LIBUTI <- freq_plot('LIBUTI')
+PAYPVN <- freq_plot('PAYPVN')
+CODMRQ <- freq_plot('CODMRQ')
+LIBMRQ <- freq_plot('LIBMRQ')
+TYPUSI <- freq_plot('TYPUSI')
+TYPCOM <- freq_plot('TYPCOM')
+PVRNUM <- freq_plot('PVRNUM')
+PVRVAR <- freq_plot('PVRVAR')
+PVRVER <- freq_plot('PVRVER')
+I4X4   <- freq_plot('I4X4')
+TYPMOT <- freq_plot('TYPMOT')
+CODCRB <- freq_plot('CODCRB')
+LIBCRB <- freq_plot('LIBCRB')
+INFOUTI <- freq_plot('INFOUTI')
+EUNORM  <- freq_plot('EUNORM')
 
-CATSTC2$CATSTC[which(CATSTC2$Prop < 5)] <- "Other"
+num_cols <- select(filter(dict, Column.class != 'character'), Column.name)
+
+paste(char_cols$Column.name, " <- ", "freq_plot('",char_cols$Column.name, "')", sep = "")
+
+
+
 
 ggplot(data = vehcln, aes(x = CATSTC)) +
   geom_bar()
@@ -37,4 +86,3 @@ ggsave(plot = last_plot(),
 # Summary statistics of continuous variables
 
 # Histograms of continuous variables
-
